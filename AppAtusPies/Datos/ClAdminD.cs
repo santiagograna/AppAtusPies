16ea29ef166f5c33d16d5df832b3f1c958c5d298
santiagograna <david.granados@soy.sena.edu.co>
2024-11-13 19:29:39 -0500

    Creacion de la Clase ClAdminD
    Cree la clase ClAdminD que contiene todos los parametros a recibir para el registro del vendedor , el llamamiento al procedimiento almacenado

---
+++ AppAtusPies/Datos/ClAdminD.cs
@@ -0,0 +1,32 @@
+﻿using AppAtusPies.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AppAtusPies.Datos
+{
+    public class ClAdminD
+    {
+        public ClAdminE MtdRegistrarVendedor(ClAdminE oDatos)
+        {
+            ClConexion oconexion = new ClConexion();
+
+            SqlCommand cmd = new SqlCommand("spRegistraVendedor", oconexion.MtdAbrirConexion());
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@nombre", oDatos.nombre);
+            cmd.Parameters.AddWithValue("@documento", oDatos.documento);
+            cmd.Parameters.AddWithValue("@telefono", oDatos.telefono);
+            cmd.Parameters.AddWithValue("@email", oDatos.email);
+            cmd.Parameters.AddWithValue("@estado", oDatos.estado);
+            cmd.Parameters.AddWithValue("@password", oDatos.password);
+
+            cmd.ExecuteNonQuery();
+            oconexion.MtdCerrarConexion();
+
+            return oDatos;
+
+        }
+    }
+}
