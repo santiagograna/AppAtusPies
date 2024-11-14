6c8c65f6149606494b648d9ebe8eb1ea6ee2893f
santiagograna <david.granados@soy.sena.edu.co>
2024-11-13 20:30:03 -0500

    Creacion de la pagina Maestra de Admin
    Cree la pagina Maestra del administrador que contiene las funciones de registrar vendedores y demas funciones

---
+++ AppAtusPies/Vista/MaestraAdmin.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppAtusPies.Vista
+{
+    public partial class MaestraAdmin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
