873a3e66cd66ec9d2e64854c966ff5f5fafd6d7f
santiagograna <david.granados@soy.sena.edu.co>
2024-11-13 19:22:13 -0500

    Creacion de la carpeta entidades
    cree la carpeta entidades que contiene todo lo relacionado con el administrador

---
+++ AppAtusPies/Entidades/ClAdminE.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppAtusPies.Entidades
+{
+    public class ClAdminE
+    {
+        public int idVendedor { get; set; }
+        public string nombre { get; set; }
+        public string documento { get; set; }
+        public string email { get; set; }
+        public string telefono { get; set; }
+        public string estado { get; set; }
+        public string password { get; set; }
+    }
+}
