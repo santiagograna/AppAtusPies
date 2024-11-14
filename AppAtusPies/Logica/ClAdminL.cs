ede9ca44cb79cd79eee9b18e05b6917b933a7091
santiagograna <david.granados@soy.sena.edu.co>
2024-11-13 19:42:06 -0500

    Creacion de la carpeta Logica
    Cree la carpeta Logica que contiene la clase CladminL donde tiene el llamado a la clase CladminD y la encriptacion de la contraseña

---
+++ AppAtusPies/Logica/ClAdminL.cs
@@ -0,0 +1,37 @@
+﻿using AppAtusPies.Datos;
+using AppAtusPies.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace AppAtusPies.Logica
+{
+    public class ClAdminL
+    {
+        public ClAdminE MtdRegistroVendedor(ClAdminE oDatos)
+        {
+            // Encriptar la contraseña
+            oDatos.password = HashPassword(oDatos.password);
+
+            ClAdminD oDatosD = new ClAdminD();
+            return oDatosD.MtdRegistrarVendedor(oDatos);
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
